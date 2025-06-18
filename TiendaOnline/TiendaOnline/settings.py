58ad591e352efe5a7012788138d98a4028b99f61
Andres Abdala Alvarez <andrespampa@gmail.com>
2025-06-18 01:20:23 -0300

    terminando envio de mail via formulario

--- TiendaOnline/TiendaOnline/settings.py
+++ TiendaOnline/TiendaOnline/settings.py
@@ -129,6 +129,15 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+#protocolo de seguridad
+EMAIL_USE_TLS = True
+#Puerto que se va a usar
+EIMAL_PORT = 587
+EMAIL_HOST_USER = 'mail@gmail.com'
+EMAIL_HOST_PASSWORD = 'password'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
--- TiendaOnline/gestionPedidos/views.py
+++ TiendaOnline/gestionPedidos/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from gestionPedidos.models import *
+from django.conf import settings  # Para acceder a las configuraciones del proyecto
+from django.core.mail import send_mail
 
 # Create your views here.
 
@@ -13,9 +15,37 @@ def buscar(request):
     if request.GET['prd']:
         # mensaje = f'Articulo buscado: {request.GET['prd']}' #prd corresponde al nombre del input en el formulario
         producto = request.GET['prd']
-        articulos = Articulos.objects.filter(nombre__icontains=producto)  # Filtra los artículos que contienen el texto buscado. icontains = like de sql
-        return render(request, 'resultados_busqueda.html', {'articulos': articulos, 'query': producto})
+
+        #Limitar los caracteres a buscar
+        if len(producto) > 20:
+            mensaje = 'Texto de busqueda demasiado largo'
+        else:
+            articulos = Articulos.objects.filter(nombre__icontains=producto)  # Filtra los artículos que contienen el texto buscado. icontains = like de sql
+            return render(request, 'resultados_busqueda.html', {'articulos': articulos, 'query': producto})
+    
     else:
         mensaje = 'No has introducido nada en el buscador'
     
     return HttpResponse(mensaje)
+
+
+
+
+def contacto(request):
+
+    if request.method == 'POST':
+
+        subject = request.POST['asunto']
+        message = request.POST['mensaje'] + request.POST['email']
+
+        #Email no de donde viene. NO confundir con el email que nos envia el usuario
+        email_from = settings.EMAIL_HOST_USER
+
+        #todas las direcciones de correo a las que se enviara el mensaje
+        recipient_list = ["mail@mail.com"]
+
+        send_mail(subject, message, email_from, recipient_list)
+
+        return render(request, 'gracias.html')
+
+    return render(request, 'contacto.html')
