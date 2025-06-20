84b0f570174db83332731a2090573f4d6233e031
Andres Abdala Alvarez <andrespampa@gmail.com>
2025-06-19 21:24:26 -0300

    Terminando Proyecto Web 3 - Video 28

---
+++ ProyectoWeb/ProyectoWebApp/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+# Create your views here.
+
+#Tengo que crear tantas vistas como paginas tenga el proyecto
+
+def home(request):
+    return render(request, 'ProyectoWebApp/home.html')
+
+def servicios(request):
+    return render(request, 'ProyectoWebApp/servicios.html')
+
+def tienda(request):
+    return render(request, 'ProyectoWebApp/tienda.html')
+
+def blog(request):
+    return render(request, 'ProyectoWebApp/blog.html')
+
+def contacto(request):
+    return render(request, 'ProyectoWebApp/contacto.html')
