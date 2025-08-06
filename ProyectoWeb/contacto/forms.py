ba1244941e853d6e42c76ea9f9f4e2d05bfc59a8
Andres Abdala Alvarez <andrespampa@gmail.com>
2025-08-05 22:40:55 -0300

    Terminando con estetica del fomrulario y csrf - Video 44

--- ProyectoWeb/contacto/forms.py
+++ ProyectoWeb/contacto/forms.py
@@ -3,4 +3,4 @@ from django import forms
 class FormularioContacto(forms.Form):
     nombre = forms.CharField(label='Nombre', max_length=50, required=True)
     email = forms.CharField(label='Email', max_length=50, required=True)
-    contenido = forms.CharField(label='Contenido', max_length=200,)
+    contenido = forms.CharField(label='Contenido', max_length=200, widget=forms.Textarea)
